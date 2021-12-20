c47929db89466e28ec3bec78a17a5e66a1988a0a
Marco Ris <ris@contria.com>
2021-12-20 15:27:46 +0100

    Update template and quote controller

--- app/src/Controller/QuoteController.php
+++ app/src/Controller/QuoteController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuoteController extends AbstractController
-{
-    #[Route('/quote', name: 'quote')]
-    public function index(): Response
-    {
-        return $this->render('quote/index.html.twig', [
-            'controller_name' => 'QuoteController',
-        ]);
+class QuoteController extends AbstractController {
+    #[Route('/', name: 'index')]
+    public function index(
+        QuoteRepository $quoteRepository
+    )
+    : Response {
+
+        return $this->render(
+            'quote/index.html.twig',
+            [
+                'quotes' => $quoteRepository->findAll(),
+            ]
+        );
     }
 }
